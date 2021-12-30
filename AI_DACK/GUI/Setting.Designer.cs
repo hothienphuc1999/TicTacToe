15d54e0b0808205cfa8d2a3e743f95e84fe3e4cf
Hồ Thiên Phúc <50455561+hothienphuc1999@users.noreply.github.com>
2021-12-30 19:17:21 +0700

    Add files via upload

---
+++ AI_DACK/GUI/Setting.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace AI_DACK
+{
+    partial class Setting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.mode1RadioButton = new System.Windows.Forms.RadioButton();
+            this.mode2RadioButton = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.trackBar1.Location = new System.Drawing.Point(105, 5);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Minimum = 3;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(274, 29);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(384, 39);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(26, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "3";
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(5, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Số ô ngang và dọc";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.radioButton3);
+            this.panel3.Controls.Add(this.radioButton1);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 39);
+            this.panel3.Margin = new System.Windows.Forms.Padding(5);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(5);
+            this.panel3.Size = new System.Drawing.Size(384, 59);
+            this.panel3.TabIndex = 3;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(73, 33);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(39, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Dễ";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(73, 10);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(44, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Khó";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label3.Location = new System.Drawing.Point(5, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 49);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Độ khó";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Location = new System.Drawing.Point(0, 148);
+            this.panel4.Name = "panel4";
+            this.panel4.Padding = new System.Windows.Forms.Padding(5, 5, 10, 5);
+            this.panel4.Size = new System.Drawing.Size(384, 34);
+            this.panel4.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(230, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Tạo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(299, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hủy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.mode1RadioButton);
+            this.panel2.Controls.Add(this.mode2RadioButton);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 98);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(5);
+            this.panel2.Size = new System.Drawing.Size(384, 52);
+            this.panel2.TabIndex = 5;
+            // 
+            // mode1RadioButton
+            // 
+            this.mode1RadioButton.AutoSize = true;
+            this.mode1RadioButton.Checked = true;
+            this.mode1RadioButton.Location = new System.Drawing.Point(73, 8);
+            this.mode1RadioButton.Name = "mode1RadioButton";
+            this.mode1RadioButton.Size = new System.Drawing.Size(45, 17);
+            this.mode1RadioButton.TabIndex = 6;
+            this.mode1RadioButton.TabStop = true;
+            this.mode1RadioButton.Text = "PvP";
+            this.mode1RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // mode2RadioButton
+            // 
+            this.mode2RadioButton.AutoSize = true;
+            this.mode2RadioButton.Location = new System.Drawing.Point(73, 27);
+            this.mode2RadioButton.Name = "mode2RadioButton";
+            this.mode2RadioButton.Size = new System.Drawing.Size(45, 17);
+            this.mode2RadioButton.TabIndex = 5;
+            this.mode2RadioButton.TabStop = true;
+            this.mode2RadioButton.Text = "PvC";
+            this.mode2RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Location = new System.Drawing.Point(5, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 42);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Chế độ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Setting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(384, 182);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Setting";
+            this.Text = "Tạo ván mới";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton mode1RadioButton;
+        private System.Windows.Forms.RadioButton mode2RadioButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ AI_DACK/Libs/AlphaBeta.cs
@@ -0,0 +1,201 @@
+﻿using AI_DACK.libs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_DACK.Libs
+{
+    class AlphaBeta
+    {
+        int n = 0;
+        const int INFINITY = 100;
+        private int _numOfRow;
+        private int _numOfCol;
+        private Dictionary<char, int> _scoreTable;
+        private int _level;
+
+        public int NumOfRow { get => _numOfRow; set => _numOfRow = value; }
+        public int NumOfCol { get => _numOfCol; set => _numOfCol = value; }
+        public Dictionary<char, int> ScoreTable { get => _scoreTable; set => _scoreTable = value; }
+        public int Level { get => _level; set => _level = value; }
+
+        public AlphaBeta(int numOfRow, int numOfCol, int level)
+        {
+            _numOfRow = numOfRow;
+            _numOfCol = numOfCol;
+            _level = level;
+            if (level == 0)
+            {
+                _scoreTable = new Dictionary<char, int>()
+                {
+                    {'t',0},
+                    {'X',1},
+                    {'O',-1},
+                    {'n',-10},
+                };
+            }
+            else
+            {
+                _scoreTable = new Dictionary<char, int>()
+                {
+                    {'t',0},
+                    {'X',5},
+                    {'O',-5},
+                    {'n',-10},
+                };
+            }
+        }
+
+        char CheckWin(char[,] content)
+        {
+            // ngang
+            for (int i = 0; i < _numOfRow; i++)
+                for (int j = 0; j < _numOfCol - 2; j++)
+                    if (content[i, j] == content[i, j + 1] && content[i, j + 1] == content[i, j + 2] && content[i, j] != '-')
+                        return content[i, j];
+
+            // doc
+            for (int i = 0; i < _numOfCol; i++)
+                for (int j = 0; j < _numOfRow - 2; j++)
+                    if (content[j, i] == content[j + 1, i] && content[j + 1, i] == content[j + 2, i] && content[j, i] != '-')
+                        return content[j, i];
+
+            // cheo 1
+            for (int i = 0; i < _numOfRow - 2; i++)
+                for (int j = 0; j < _numOfCol - 2; j++)
+                    if (content[i, j] == content[i + 1, j + 1] && content[i + 1, j + 1] == content[i + 2, j + 2] && content[i, j] != '-')
+                        return content[i, j];
+
+            // cheo 2 
+            for (int i = 0; i < _numOfRow - 2; i++)
+                for (int j = _numOfCol - 1; j > 1; j--)
+                    if (content[i, j] == content[i + 1, j - 1] && content[i + 1, j - 1] == content[i + 2, j - 2] && content[i, j] != '-')
+                        return content[i, j];
+
+            // not win
+            for (int i = 0; i < _numOfRow; i++)
+                for (int j = 0; j < _numOfCol; j++)
+                    if (content[i, j] == '-')
+                        return 'n'; //null
+
+            return 't'; // tie
+        }
+
+        int Score(char[,] state)
+        {
+            char result = CheckWin(state);
+            return _scoreTable[result];
+        }
+
+        List<Tuple<int, int>> Action(char[,] state)
+        {
+            List<Tuple<int, int>> result = new List<Tuple<int, int>>();
+            for (int i = 0; i < _numOfRow; i++)
+            {
+                for (int j = 0; j < _numOfCol; j++)
+                {
+                    if (state[i, j] == '-')
+                    {
+                        result.Add(Tuple.Create<int, int>(i, j));
+                    }
+                }
+            }
+            return result;
+        }
+
+        // Alpha Beta Pruning
+
+        int Max_value(char[,] state, int step, ref int alpha, ref int beta)
+        {
+            n++;
+
+            step++;
+            int scr = Score(state);
+
+            int value = -INFINITY;
+            if (scr != -10)
+                return _level == 1 ? scr + step : scr; // lay min trong tat ca max
+            foreach (Tuple<int, int> act in Action(state))
+            {
+                char[,] newState = Helper.DeepCopyChar(state);
+
+                newState[act.Item1, act.Item2] = 'X';
+                value = Math.Max(value, Min_value(newState, step, ref alpha, ref beta));
+                if (value >= beta)
+                    return value;
+                alpha = Math.Max(alpha, value);
+            }
+            return value;
+        }
+
+        int Min_value(char[,] state, int step, ref int alpha, ref int beta)
+        {
+            n++;
+
+            step++;
+            int scr = Score(state);
+
+            int value = INFINITY;
+            if (scr != -10)
+                return _level == 1 ? scr + step : scr; // lay max trong tat ca min
+            foreach (Tuple<int, int> act in Action(state))
+            {
+                char[,] newState = Helper.DeepCopyChar(state);
+
+                newState[act.Item1, act.Item2] = 'O';
+                value = Math.Min(value, Max_value(newState, step, ref alpha, ref beta));
+                if (value <= alpha)
+                    return value;
+                beta = Math.Min(beta, value);
+            }
+            return value;
+        }
+
+        public Tuple<int, int> AlphaBetaDecision(Board board, char turn)
+        {
+            int positveInf = 100;
+            int negativeInf = -100;
+
+            List<Tuple<string, int>> ketqua = new List<Tuple<string, int>>();
+            Tuple<int, int> decision = null;
+
+            if (turn == 'X')
+            {
+                int value = -INFINITY;
+                foreach (Tuple<int, int> act in Action(board.Content))
+                {
+                    char[,] state = Helper.DeepCopyChar(board.Content);
+                    state[act.Item1, act.Item2] = 'X';
+
+                    int result = Min_value(state, 1, ref negativeInf, ref positveInf);
+                    if (result > value)
+                    {
+                        value = result;
+                        decision = act;
+                    }
+                }
+            }
+            else
+            {
+                int value = INFINITY;
+                foreach (Tuple<int, int> act in Action(board.Content))
+                {
+                    char[,] state = Helper.DeepCopyChar(board.Content);
+                    state[act.Item1, act.Item2] = 'O';
+
+                    int result = Max_value(state, 1, ref negativeInf, ref positveInf);
+                    ketqua.Add(new Tuple<string, int>(act.ToString(), result));
+                    if (result < value)
+                    {
+                        value = result;
+                        decision = act;
+                    }
+                }
+            }
+            return decision;
+        }
+    }
+}
+
